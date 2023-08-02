33651f04403507a78ef08198239923e03f28e959
kaloyantk <kaloyan_tihomirov@icloud.com>
2023-08-02 03:04:58 +0300

    Added test to ensure the controller sends the correct pagination data to the view - the test passes

--- SportsStore.Tests/HomeControllerTests.cs
+++ SportsStore.Tests/HomeControllerTests.cs
@@ -3,6 +3,7 @@ using Moq;
 using SportsStore.Controllers;
 using SportsStore.Data;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Tests
 {
@@ -61,6 +62,37 @@ namespace SportsStore.Tests
             Assert.True(prodArray.Length == 2);
             Assert.Equal("P4", prodArray[0].Name);
             Assert.Equal("P5", prodArray[1].Name);
+        }
+
+        [Fact]
+        public void Can_Send_Pagination_View_Model()
+        {
+
+            // Arrange
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+                new Product {ProductId = 1, Name = "P1"},
+                new Product {ProductId = 2, Name = "P2"},
+                new Product {ProductId = 3, Name = "P3"},
+                new Product {ProductId = 4, Name = "P4"},
+                new Product {ProductId = 5, Name = "P5"}
+            }).AsQueryable<Product>());
+
+            // Arrange
+            HomeController controller =
+                new HomeController(mock.Object) { PageSize = 3 };
+
+            // Act
+            ProductsListViewModel result =
+                (controller.Index(2) as ViewResult)?.ViewData.Model as
+                   ProductsListViewModel ?? new();
+
+            // Assert
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.Equal(2, pageInfo.CurrentPage);
+            Assert.Equal(3, pageInfo.ItemsPerPage);
+            Assert.Equal(5, pageInfo.TotalItems);
+            Assert.Equal(2, pageInfo.TotalPages);
         }
     }
 }
