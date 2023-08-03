f4b08bfeaf78c1b488f2b1ae19051bc62ac5d5f1
kaloyantk <kaloyan_tihomirov@icloud.com>
2023-08-04 01:58:48 +0300

    Added administration with Blazor

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddSession();
 builder.Services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
+builder.Services.AddServerSideBlazor();
 
 //ASP.NET Core receives HTTP requests and passes them along a request pipeline, which is populated with middleware components registered using the app property.
 var app = builder.Build();
@@ -48,6 +49,9 @@ new { Controller = "Home", action = "Index", productPage = 1 });
 
 app.MapDefaultControllerRoute();
 app.MapRazorPages();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/admin/{*catchall}", "/Admin/Index");
 
 SeedData.EnsurePopulated(app);
 
