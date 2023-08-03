fe9b5a8031af42091cef6d4fecb86397f1013019
kaloyantk <kaloyan_tihomirov@icloud.com>
2023-08-03 22:29:38 +0300

    Users can choose category from the navbar, added testing for the view component

---
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using SportsStore.Data;
+
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IStoreRepository repository;
+
+        public NavigationMenuViewComponent(IStoreRepository repo)
+        {
+            this.repository = repo;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedCategory = RouteData?.Values["category"];
+            return View(this.repository
+                .Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
--- SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -27,6 +27,10 @@ namespace SportsStore.Infrastructure
 
         public string? PageAction { get; set; }
 
+        [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
+        public Dictionary<string, object> PageUrlValues { get; set; }
+                        = new Dictionary<string, object>();
+
 
         public bool PageClassesEnabled { get; set; } = false;
 
@@ -50,8 +54,9 @@ namespace SportsStore.Infrastructure
                 for (int i = 1; i <= PageModel.TotalPages; i++)
                 {
                     TagBuilder tag = new TagBuilder("a");
-                    tag.Attributes["href"] = urlHelper.Action(PageAction,
-                       new { productPage = i });
+                    PageUrlValues["productPage"] = i;
+                    tag.Attributes["href"] = urlHelper.Action(PageAction, PageUrlValues);
+                    tag.Attributes["href"] = urlHelper.Action(PageAction, new { productPage = i });
                     if (PageClassesEnabled)
                     {
                         tag.AddCssClass(PageClass);
