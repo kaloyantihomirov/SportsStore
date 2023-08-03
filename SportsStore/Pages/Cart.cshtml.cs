e39c10af351106e87fd1dbaff4d0fbb362dfe3a8
kaloyantk <kaloyan_tihomirov@icloud.com>
2023-08-03 22:52:12 +0300

    Implemented basic functions of the shopping cart

--- SportsStore/Pages/Cart.cshtml.cs
+++ SportsStore/Pages/Cart.cshtml.cs
@@ -1,12 +1,44 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using SportsStore.Data;
+using SportsStore.Infrastructure;
+using SportsStore.Models;
+
 namespace SportsStore.Pages
 {
     public class CartModel : PageModel
     {
-        public void OnGet()
+        private IStoreRepository repository;
+
+        public CartModel(IStoreRepository repo)
         {
+            this.repository = repo;
+        }
+
+        public Cart? Cart { get; set; }
+
+        public string ReturnUrl { get; set; } = "/";
+
+        public void OnGet(string returnUrl)
+        {
+            this.ReturnUrl = returnUrl ?? "/";
+            this.Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+        }
+        public IActionResult OnPost(long productId, string returnUrl)
+        {
+            Product? product = this.repository
+                .Products
+                .FirstOrDefault(p => p.ProductId == productId);
+
+            if (product != null)
+            {
+                this.Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+                this.Cart.AddItem(product, 1);
+                HttpContext.Session.SetJson("cart", Cart);
+            }
+
+            return RedirectToPage(new { returnUrl = returnUrl });
         }
     }
 }
