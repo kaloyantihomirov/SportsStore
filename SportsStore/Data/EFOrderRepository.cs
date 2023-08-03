45fd037cfcde7ec84fed67aa30d9c6e800778984
kaloyantk <kaloyan_tihomirov@icloud.com>
2023-08-04 01:28:50 +0300

    Added a repo for orders

---
+++ SportsStore/Data/EFOrderRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
+namespace SportsStore.Data
+{
+    public class EFOrderRepository : IOrderRepository
+    {
+        private StoreDbContext context;
+
+        public EFOrderRepository(StoreDbContext ctx)
+        {
+            this.context = ctx;
+        }
+
+        public IQueryable<Order> Orders => this.context
+            .Orders
+            .Include(o => o.Lines)
+            .ThenInclude(l => l.Product);
+
+        public void SaveOrder(Order order)
+        {
+            context.AttachRange(order.Lines.Select(l => l.Product));
+
+            if (order.OrderID == 0)
+            {
+                context.Orders.Add(order);
+            }
+        }
+    }
+}
---
+++ SportsStore/Data/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using SportsStore.Models;
+
+namespace SportsStore.Data
+{
+    public interface IOrderRepository
+    {
+        IQueryable<Order> Orders { get; }
+
+        void SaveOrder(Order order);
+    }
+}
